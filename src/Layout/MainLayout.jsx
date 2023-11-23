ca7f62e3c3eb8f9191bfee7530b9b81770730ad2
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-24 01:09:25 +0600

    error page added

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+
+import { Outlet } from 'react-router-dom';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div>
+                <h1>This is main layout</h1>
+            </div>
+        </div>
+    );
+};
+
+export default MainLayout;
