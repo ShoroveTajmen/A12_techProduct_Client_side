215ccaa94200e92cbaa3750b27978b08c0877dba
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-24 13:40:04 +0600

    Private Route Added

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
                   className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
                 >
                   <li>
-                    <a className="justify-between"> {user.displayName}</a>
+                    <h1 className="justify-between"> {user.displayName}</h1>
                   </li>
                   <li>
                     <a>DashBoard</a>
---
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+  console.log(loading);
+
+  if (loading) {
+    return <span className="loading loading-infinity loading-lg"></span>;
+  }
+
+  if (user?.email) {
+    return children;
+  }
+  return <Navigate state={location.pathname} to="/login"></Navigate>;
+};
+
+export default PrivateRoute;
