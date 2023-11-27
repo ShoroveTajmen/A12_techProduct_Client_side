a22a2de36af6a06a2e394a6ac6c7e6b706cdfc60
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-27 23:31:50 +0600

    reported product delete functionality added

--- src/Pages/Dashboard/ReportedProduct/ReportedProduct.jsx
+++ src/Pages/Dashboard/ReportedProduct/ReportedProduct.jsx
@@ -1,18 +1,18 @@
-
 import { Link } from "react-router-dom";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useAuth from "../../../hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const ReportedProduct = () => {
   const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
 
-  //using tanstack query to get data
+  //using tanstack query to get reported products data
   const { data, refetch, isLoading } = useQuery({
-    queryKey: ["userProducts"],
+    queryKey: ["reportedProducts"],
     queryFn: async () => {
-      const res = await axiosSecure.get(`/allProducts?status=pending`);
+      const res = await axiosSecure.get("/reportedPoducts");
       console.log(res.data);
       return res.data;
     },
@@ -21,6 +21,35 @@ const ReportedProduct = () => {
   if (isLoading) {
     return <p>Hello</p>;
   }
+
+  const handleDeleteItem = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/product/${item._id}`);
+        // console.log(res.data);
+        if (res.data.deletedCount > 0) {
+          //refetch to update the ui
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `Reported Product deleted successfully`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      }
+    });
+  };
+
   return (
     <div>
       <div>
@@ -52,7 +81,7 @@ const ReportedProduct = () => {
                   <td className="text-left">
                     {" "}
                     <button
-                      //   onClick={() => handleMarksFeatured(item?._id)}
+                      onClick={() => handleDeleteItem(item)}
                       className="btn bg-red-600 text-white border-none"
                     >
                       Delete
